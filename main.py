21bf397738483f9da4bd7ee2438fe8f3e159f53f
Timofii0507 <timofiiyurkov@gamil.com>
2023-11-05 19:04:58 +0200

    Перше завдання з дз

---
+++ main.py
@@ -0,0 +1,6 @@
+def display_quote():
+    quote = "Don't compare yourself with anyone in this world…\nif you do so, you are insulting yourself."
+    author = "Bill Gates"
+    formatted_text = f'"{quote}"\n{author}'
+    print(formatted_text)
+display_quote()
