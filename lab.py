39acad57e73dbf07e62f2796f0313fb81e62224f
Timofii0507 <timofiiyurkov@gamil.com>
2023-12-20 21:24:20 +0200

    Перше завдання з  практичної роботи

---
+++ lab.py
@@ -0,0 +1,6 @@
+def print_quote():
+    quote = """Don't let the noise of others' opinions
+drown out your own inner voice."""
+    quote = quote + f"\n{'Steve Jobs'}"
+    print(quote)
+print_quote()
