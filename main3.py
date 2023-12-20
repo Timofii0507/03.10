82c987c6d92c4f838881b958f02fa608fb851991
Timofii0507 <timofiiyurkov@gamil.com>
2023-12-20 18:35:27 +0200

    Третє завдання з дз

--- main3.py
+++ main3.py
@@ -1,12 +1,13 @@
-def draw_line(length, direction, symbol):
-    if direction == 'горизонтальна':
-        print(symbol * length)
-    elif direction == 'вертикальна':
-        for _ in range(length):
-            print(symbol)
+def draw_square(side_length, fill_symbol, filled):
+    if filled:
+        for _ in range(side_length):
+            print(fill_symbol * side_length)
     else:
-        print("Невірний напрямок. Використовуйте 'горизонтальна' або 'вертикальна'.")
-length = int(input("Введіть довжину лінії: "))
-direction = input("Введіть напрямок ('горизонтальна' або 'вертикальна'): ")
-symbol = input("Введіть символ: ")
-draw_line(length, direction, symbol)
+        print(fill_symbol * side_length)
+        for _ in range(side_length - 2):
+            print(fill_symbol + ' ' * (side_length - 2) + fill_symbol)
+        print(fill_symbol * side_length)
+side_length = int(input("Введіть довжину сторони квадрата (ціле число більше або рівне 2): "))
+fill_symbol = input("Введіть символ для заповнення квадрата: ")
+filled_square = input("Введіть 'True', якщо квадрат повинен бути заповненим, або 'False' в іншому випадку: ").lower() == 'true'
+draw_square(side_length, fill_symbol, filled_square)
